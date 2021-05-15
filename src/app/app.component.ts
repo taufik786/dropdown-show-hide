d26161815d47243715c99ddd3af849e91cbba77b
Taufik Alam <taufikalam759@gmail.com>
2021-05-15 20:07:21 +0530

    .

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,34 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'dropdown-test';
+  DropdownDatas = [ 'A', 'B', 'C', 'D', 'E'];
+  DropdownDatas1 = [ 'A', 'B', 'C', 'D', 'E'];
+  DropdownDatas2 = [ 'A', 'B', 'C', 'D', 'E'];
+
+  DropdownForm: FormGroup;
+  hideitem;
+  // i = this.hideitem
+
+  constructor(private fb: FormBuilder){
+    this.DropdownForm = this.fb.group({
+      dropdown1: ['', Validators.required],
+      dropdown2: ['', Validators.required],
+      dropdown3: ['', Validators.required],
+    })
+  }
+
+  onDropdownSelected(i: number){
+    this.DropdownDatas.splice(i, 0) // slice //pop
+    // this.DropdownDatas1.splice(i, 1)
+    this.DropdownDatas1.splice(i, 0)
+    this.DropdownDatas2.splice(i, 0)
+    this.hideitem = i;
+
+  }
+
+  onSubmit(){
+    console.log(this.DropdownForm.value);
+    this.DropdownForm.reset();
+
+  }
 }
